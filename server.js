ed9b3c2bdaa497fe5ae8088efbe44e85dea34ad9
Piero Rodriguez <piero@Pieros-MacBook-Pro.local>
2023-10-06 22:38:27 -0400

    joined the routes js to the server.js

--- server.js
+++ server.js
@@ -1,27 +1,60 @@
-// Dependencies
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
-// app use express
 const app = express();
 
-// creating port
 const PORT = process.env.PORT || 3001;
 
-
-// asks express to create a route for every file in the 'public' folder and give it a '/' route
-app.use(express.static('public'));
-// sets up express app to handel data parser, middle wear created req.body
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.use(express.json());
 
-
-// routes files
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
-
-
-// starts the server
-app.listen(PORT, () => {
-  console.log(`Server available at localhost ${PORT}`);
+app.post('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    var notes = JSON.parse(data);
+    let userNote = req.body;
+    userNote.id = Math.floor(Math.random() * 5000);
+    notes.push(userNote);
+  fs.writeFile('./db/db.json', JSON.stringify(notes), (err, data) => {
+      res.json(userNote);
+  });
+  }); 
 });
 
+app.delete('/api/notes/:id', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    let notes = JSON.parse(data);
+    const newNotes = notes.filter(note => note.id !== parseInt(req.params.id));
+  
+  fs.writeFile('./db/db.json', JSON.stringify(newNotes), (err, data) => {
+    res.json({msg: 'successfully'});
+  });
+});
+});
+
+app.get('api/notes/:id', (req, res) =>{
+  res.json(notes[req.params.id]);
+});
+
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    var notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/notes.html'))
+});
+
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, '/index.html'));
+});   
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+});
